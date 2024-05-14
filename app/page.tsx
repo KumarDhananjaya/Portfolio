c0f160132976b3d6481cec8a3a25bf48731dbc6a
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2024-05-14 10:54:23 +0530

    components configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import About from '@/components/About'
+import Hero from '@/components/Hero'
+import Portfolio from '@/components/Portfolio'
+import Skills from '@/components/Skills'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <main>
+      <>
+        <Hero />
+        <About/>
+        <Skills/>
+        <Portfolio/>
+      </>
+    </main>
   )
 }
 
