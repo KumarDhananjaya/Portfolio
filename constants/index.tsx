836ace40d3e7788b9233205c3dad641fb1b018ec
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2024-05-15 14:52:59 +0530

    navbar items constants

---
+++ constants/index.tsx
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    {href: '/', key: 'home', label: 'Home'},
+    {href: '/', key: 'about', label: 'About'},
+    {href: '/', key: 'skills', label: 'Skills'},
+    {href: '/', key: 'portfolio', label: 'Porfolio'},
+    {href: '/', key: 'contact_me', label: 'Contact Me'},
+];
