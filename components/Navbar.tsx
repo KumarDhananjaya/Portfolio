b762d7119def2c11cd55bae144b1149fe64cbd3a
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2024-05-15 12:11:22 +0530

    started with navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='padding-container relative z-30 rounded-lg bg-white shadow-xl ring-1 ring-slate-100 '>
+      <div className="flex-between max-container">
+          <Link href="/" className="bold-28 capitalize relative">
+              kumar<span className="text-blue-700"> dhananjaya</span>
+              <span className="absolute top-[-0.3rem] right-[-1rem] h-5 w-5 linearGradient rounded-full -z-10"></span>
+          </Link>
+      </div>
+    </nav>
   )
 }
 
